9f8290aed0b72410da9c99e7d636b895bda4ec18
Hundoas <64798579+Hundoas@users.noreply.github.com>
2020-05-04 17:34:32 +0100

    Update keymap_poker.c

--- gh60/keymap_poker.c
+++ gh60/keymap_poker.c
@@ -4,9 +4,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     KEYMAP(
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, DEL \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, 1 \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,    ENT,  2 \
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM, DOT, SLSH,     RSFT, UP, 3 \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, VOLUP \
+        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,    ENT,  VOLDOWN \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM, DOT, SLSH,     RSFT, UP, MUTE \
         LCTL,LGUI,LALT,          SPC,              FN0, RGUI, RCTL, LEFT, DOWN, RIGHT ),
    /* 1: FN0 */
     KEYMAP(
